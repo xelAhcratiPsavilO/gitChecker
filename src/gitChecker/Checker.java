43d49c3512b1c1f79dbd1ac82e51198e3c384fb9
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-06-30 20:32:54 +0100

    Send all login credentials

--- src/gitChecker/Checker.java
+++ src/gitChecker/Checker.java
@@ -23,6 +23,7 @@ public class Checker {
 		driver.findElement(By.xpath("/html/body/div[1]/header/div/div[2]/div[2]/a[1]")).click();
 		driver.findElement(By.id("login_field")).sendKeys(credentials.USERNAME);
 		driver.findElement(By.id("password")).sendKeys(credentials.PASSWORD);
+		driver.findElement(By.xpath("//*[@id=\"login\"]/form/div[3]/input[4]")).click();
 
 	}
 
