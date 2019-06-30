6dc16f3bdeef2fea739741c8435c5e1128bd23f1
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-06-30 19:31:26 +0100

    Get driver to open url

--- src/gitChecker/Checker.java
+++ src/gitChecker/Checker.java
@@ -1,9 +1,21 @@
 package gitChecker;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Checker {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// Create Driver object for Chrome browser
+		
+		System.setProperty("webdriver.chrome.driver", "chromedriver");
+		WebDriver driver = new ChromeDriver();
+		
+		// Driver actions
+		
+		driver.get("https://github.com");
 
 	}
 
