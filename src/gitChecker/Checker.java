d3d88d00beda962fd5cb9fe5a3ebffdd79ea323f
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-06-30 20:34:09 +0100

    Click user image

--- src/gitChecker/Checker.java
+++ src/gitChecker/Checker.java
@@ -24,6 +24,7 @@ public class Checker {
 		driver.findElement(By.id("login_field")).sendKeys(credentials.USERNAME);
 		driver.findElement(By.id("password")).sendKeys(credentials.PASSWORD);
 		driver.findElement(By.xpath("//*[@id=\"login\"]/form/div[3]/input[4]")).click();
+		driver.findElement(By.xpath("/html/body/div[1]/header/div[8]/details/summary/img")).click();
 
 	}
 
